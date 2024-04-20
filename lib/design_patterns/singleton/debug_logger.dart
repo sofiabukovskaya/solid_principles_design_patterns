ab9410d20d5e0661ad3c656bdac7b3255fcbd990
sofiabykovskaya <sofi454545@gmail.com>
2024-04-20 12:28:37 +0300

    singleton design pattern

---
+++ lib/design_patterns/singleton/debug_logger.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:logger/logger.dart';
+
+abstract class BaseLogger {
+  @protected
+  late Logger? _logger;
+  @protected
+  final _dateFormatter = DateFormat('H:m:s.S');
+
+  void log(message, [Object? error, StackTrace? stackTrace]) =>
+      _logger?.i(message, error: error, stackTrace: stackTrace);
+}
+
+class DebugLogger extends BaseLogger {
+  static DebugLogger? _instance;
+
+  DebugLogger._internal() {
+    Logger.level = Level.all;
+    Logger.addLogListener(_recordHandler);
+
+    _logger = Logger();
+    _instance = this;
+  }
+
+  factory DebugLogger() => _instance ?? DebugLogger._internal();
+
+  void _recordHandler(LogEvent event) =>
+      print('${_dateFormatter.format(event.time)}: ${event.message}');
+}
---
+++ lib/design_patterns/singleton/sequence_generation.dart
@@ -0,0 +1,11 @@
+/// Exercise #1
+/// 1. Create a Singleton implementation which will generate a sequence of numbers to the callers.
+/// a. The idea here is that there is only a single number/sequence generator and all the numbers follow a perfect sequence.
+/// b. So when we do a call to the generator like getNextNumber() we should get the next number in the sequence no matter how we obtained the generator.
+/// 2. Make that implementation both with eager-instantiation and with lazy-instantiation.
+/// a. Which one would you prefer to use and why?
+library;
+
+class SequenceGeneration {
+
+}
